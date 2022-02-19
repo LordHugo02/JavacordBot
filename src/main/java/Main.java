47806e5b1b9ce9ba3dacdcb5e26e66254265c596
LordHugo02 <75981726+LordHugo02@users.noreply.github.com>
2022-02-19 22:31:03 +0100

    dice and ping working

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,15 @@
 import org.javacord.api.DiscordApi;
 import org.javacord.api.DiscordApiBuilder;
 import org.javacord.api.entity.message.MessageFlag;
+import org.javacord.api.entity.message.embed.EmbedBuilder;
 import org.javacord.api.interaction.SlashCommand;
 import org.javacord.api.interaction.SlashCommandInteraction;
+
+import java.awt.*;
+import java.io.File;
+import java.math.BigInteger;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class Main
 {
@@ -27,7 +34,36 @@ public class Main
             {
                 tab[0] = tab[0].substring(1);
                 System.out.println(tab[0]);
-                event.getChannel().sendMessage(tab[0]);
+
+                switch (tab[0])
+                {
+                    case "ping":
+                        event.getChannel().sendMessage("pong!");
+                        break;
+
+                    case "dice":
+                        int lancers = -1;
+                        BigInteger t;
+                        System.out.println(tab.length);
+                        if (tab.length == 2)
+                        {
+                            lancers = Integer.parseInt(tab[1]);
+                            if (lancers <= 0)
+                                event.getChannel().sendMessage("Veuillez entrer un nombre entre 1 et " + Integer.MAX_VALUE);
+                        }
+                        else
+                            lancers = 6;
+                        int random = (int) (Math.random() * lancers) + 1;
+
+                        event.getChannel().sendMessage("Vous obtenez un "+ random + " ! \n Sur un dé à  : " + lancers + " faces");
+                        break;
+
+                    default:
+                        event.getChannel().sendMessage("désolé, la commande " + tab[0] + " n'est pas attribuée");
+                        break;
+
+
+                }
             }
         });
     }
