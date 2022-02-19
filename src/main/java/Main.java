f359aaaae170a962c9ad476d825540955d0b85f7
LordHugo02 <75981726+LordHugo02@users.noreply.github.com>
2022-02-19 18:28:47 +0100

    bot working

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,37 @@
+import org.javacord.api.DiscordApi;
+import org.javacord.api.DiscordApiBuilder;
+import org.javacord.api.entity.message.MessageFlag;
+import org.javacord.api.interaction.SlashCommand;
+import org.javacord.api.interaction.SlashCommandInteraction;
+
 public class Main
 {
+
+    static PrivateToken t = new PrivateToken();
+    static String token = t.getToken();
+    static char prefix = '*';
+
+
     public static void main(String[] args)
     {
 
+        System.out.println("Initialisation");
+        DiscordApi api = new DiscordApiBuilder().setToken(token).login().join();
+        System.out.println("Initialisation done");
+
+
+        api.addMessageCreateListener(event ->
+        {
+            String [] tab = event.getMessageContent().split(" ");
+            if (tab[0].charAt(0) == prefix)
+            {
+                tab[0] = tab[0].substring(1);
+                System.out.println(tab[0]);
+                event.getChannel().sendMessage(tab[0]);
+            }
+        });
     }
+
+
 }
+
