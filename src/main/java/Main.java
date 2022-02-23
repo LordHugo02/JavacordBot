f6b316d21d9f088b47b7d0680e3372b4fd0162b7
LordHugo02 <75981726+LordHugo02@users.noreply.github.com>
2022-02-23 14:03:53 +0100

    added a limit to the dice command

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,8 @@
 import org.javacord.api.DiscordApi;
 import org.javacord.api.DiscordApiBuilder;
-import org.javacord.api.entity.message.MessageFlag;
 import org.javacord.api.entity.message.embed.EmbedBuilder;
-import org.javacord.api.interaction.SlashCommand;
-import org.javacord.api.interaction.SlashCommandInteraction;
 
 import java.awt.*;
-import java.io.File;
-import java.math.BigInteger;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -42,21 +37,47 @@ public class Main
                         break;
 
                     case "dice":
-                        int lancers = -1;
-                        BigInteger t;
+                        int lancers = 0;
                         System.out.println(tab.length);
                         if (tab.length == 2)
                         {
-                            lancers = Integer.parseInt(tab[1]);
-                            if (lancers <= 0)
-                                event.getChannel().sendMessage("Veuillez entrer un nombre entre 1 et " + Integer.MAX_VALUE);
+                            if (tab[1].length() > 9)
+                                event.getChannel().sendMessage("Veuillez entrer un nombre entre 1 et (10^10)-1");
+                            else
+                                lancers = Integer.parseInt(tab[1]);
+
                         }
                         else
                             lancers = 6;
                         int random = (int) (Math.random() * lancers) + 1;
 
-                        event.getChannel().sendMessage("Vous obtenez un "+ random + " ! \n Sur un dé à  : " + lancers + " faces");
+                        if (lancers !=0)
+                            event.getChannel().sendMessage("Vous obtenez un "+ random + " ! \n Sur un dé à  : " + lancers + " faces");
                         break;
+
+
+                    case "img":
+
+
+                        EmbedBuilder embed = null;
+                        try {
+                        embed = new EmbedBuilder()
+                                .setTitle("Title")
+                                .setDescription("Description")
+                                .setAuthor(event.getMessageAuthor().getDisplayName(), null,  event.getMessageAuthor().getAvatar())
+                                .addField("A field", "Some text inside the field")
+                                .addInlineField("An inline field", "More text")
+                                .addInlineField("Another inline field", "Even more text")
+                                .setColor(Color.BLUE)
+                                .setFooter("Footer", "https://cdn.discordapp.com/embed/avatars/1.png")
+                                .setImage(String.valueOf(new URL("https://picsum.photos/200/300")));
+                    } catch (MalformedURLException e) {
+                        e.printStackTrace();
+                    }
+                        // Send the embed
+                        event.getChannel().sendMessage(embed);
+
+                            break;
 
                     default:
                         event.getChannel().sendMessage("désolé, la commande " + tab[0] + " n'est pas attribuée");
